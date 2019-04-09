450d542a2c53a125a7b865c8ca507839b7fdcfdd
kasia <kgbsz@wp.pl>
2019-04-09 18:32:48 +0200

    zmiana konfiguracji z baza danych tabela myslq w wordbeach haslo, nazwa uzytkownika,nazwa bazy zaciagniety maven     utworzaony xml     Hibernate     bazy danych     java

--- src/main/java/MainNotatka.java
+++ src/main/java/MainNotatka.java
@@ -1,3 +1,5 @@
 //public class MainNotatka {
     // persistence - przetrwac,
 //}
+/***
+ *  <property name="hibernate.hbm2ddl.auto" value="create"/>  //*** co zrobic kiedy odpalamy program i create mowi stworz mi baze danych**//
